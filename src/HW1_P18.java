24d0d96eda308fdd70d83554b032858522bb7786
Leerainsummer <anthony60503@gmail.com>
2026-09-24 00:37:19 +0800

    Add files via upload

---
+++ src/HW1_P18.java
@@ -0,0 +1,11 @@
+public class HW1_P18 {
+    public static void main(String[] args) {
+        // 這是 Sample4 的部分
+        System.out.println("顯示出反斜線:\\");
+        System.out.println("顯示出單引號\'");
+        
+        // 這是 Sample5 的部分
+        System.out.println("八進位數101的字元是\101");
+        System.out.println("十六進位數0061的字元是\u0061");
+    }
+}
